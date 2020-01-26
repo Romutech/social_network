2aa7b1846052c1f29a40d58aba9840af8e32753a
Robin Muller <robin.muller.romutech@gmail.com>
2020-01-26 23:27:58 +0100

    feature: profile model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Profile(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname  = models.CharField(max_length=100)
+    user      = models.ForeignKey('User', on_delete=models.CASCADE) #Todo manage User model
+    date      = models.DateTimeField(default=timezone.now, verbose_name="Date de création")
+
+    class Meta:
+        verbose_name="Profil"
+
+    def __str__(self):
+        return "profil de {} {}".format(self.firstname, self.lastname)
