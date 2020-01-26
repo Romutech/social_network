72fa49835abe8b560c82d3bdcc7b1fc1e2eaf6f8
Robin Muller <robin.muller.romutech@gmail.com>
2020-01-26 23:37:49 +0100

    feature: comment model

--- social/models.py
+++ social/models.py
@@ -24,3 +24,15 @@ class Status(models.Model):
 
     def __str__(self):
         return self.content
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    status  = models.ForeignKey('Status', on_delete=models.CASCADE)
+    date    = models.DateTimeField(default=timezone.now, verbose_name="Date de création")
+
+    class Meta:
+        verbose_name="Commentaire"
+
+    def __str__(self):
+        return self.content
