d287c78d4c91d8024fdfe8831e49b8b18d6b0ad9
Robin Muller <robin.muller.romutech@gmail.com>
2020-01-26 23:34:40 +0100

    feature: status model

--- social/models.py
+++ social/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Profile(models.Model):
     firstname = models.CharField(max_length=100)
     lastname  = models.CharField(max_length=100)
-    user      = models.ForeignKey('User', on_delete=models.CASCADE) #Todo manage User model
+    #user     = models.ForeignKey('User', on_delete=models.CASCADE) #Todo manage User model
     date      = models.DateTimeField(default=timezone.now, verbose_name="Date de création")
 
     class Meta:
@@ -12,3 +12,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return "profil de {} {}".format(self.firstname, self.lastname)
+
+
+class Status(models.Model):
+    content = models.CharField(max_length=100)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    date    = models.DateTimeField(default=timezone.now, verbose_name="Date de création")
+
+    class Meta:
+        verbose_name="Statut"
+
+    def __str__(self):
+        return self.content
