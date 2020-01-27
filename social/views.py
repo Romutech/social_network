d560ab3d5594f5f4d8d99ae441d8fec1fb19384c
Robin Muller <robin.muller.romutech@gmail.com>
2020-01-27 01:12:22 +0100

    feature: profile_list view and template

--- social/views.py
+++ social/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile, Status, Comment
 
 def list_profiles(request):
-
+    profiles = Profile.objects.all()
     return render(request, 'social/list_profiles.html', locals())
 
 def show_profile(request, id):
