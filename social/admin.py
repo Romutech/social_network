9e8206406970e97fe579bcadbf1115d9726f9557
Robin Muller <robin.muller.romutech@gmail.com>
2020-01-27 18:16:02 +0100

    feature: user model

--- social/admin.py
+++ social/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile, Status, Comment
+from .models import Profile, Status, Comment, User
 
+admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Status)
 admin.site.register(Comment)
--- social/models.py
+++ social/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.utils import timezone
 
+class User(models.Model):
+    pseudo = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name="Utilisateur"
+
+    def __str__(self):
+        return self.pseudo
+
 class Profile(models.Model):
     firstname = models.CharField(max_length=100)
     lastname  = models.CharField(max_length=100)
-    #user     = models.ForeignKey('User', on_delete=models.CASCADE) #Todo manage User model
+    user     = models.ForeignKey('User', on_delete=models.CASCADE) #Todo manage User model
     date      = models.DateTimeField(default=timezone.now, verbose_name="Date de création")
 
     class Meta:
