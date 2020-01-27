b10a520cb13f79409cfd4e30b440db33ff720a7e
Robin Muller <robin.muller.romutech@gmail.com>
2020-01-27 21:44:03 +0100

    wip

--- social/views.py
+++ social/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.http import Http404
 from .models import Profile, Status, Comment
+from social.forms import CommentForm
 
 def list_profiles(request):
     profiles = Profile.objects.all()
@@ -12,6 +13,12 @@ def show_profile(request, id):
         profile = Profile.objects.get(id=id)
     except:
         raise Http404
+
+    form = CommentForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+
     statutes = Status.objects.filter(profile=profile)
     for status in statutes:
         comments = Comment.objects.filter(status=status)
