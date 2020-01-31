55d8b0f67a224a839a08fc47c7dc5a14a5ad802b
Robin Muller <robin.muller.romutech@gmail.com>
2020-01-31 06:11:34 +0100

    wip

--- social/forms.py
+++ social/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['content'].widget.attrs['placeholder'] = "Ajouter un commentaire"
     class Meta:
         model = Comment
         fields = ('content',)
+       
