b918436a683874c84f1044b5d39f629b8d7fced7
Robin Muller <robin.muller.romutech@gmail.com>
2020-02-04 21:35:41 +0100

    refector url

--- social/urls.py
+++ social/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profiles/', views.list_profiles, name='list_profiles'),
+    path('', views.list_profiles, name='list_profiles'),
     path('profile/<id>', views.show_profile, name='show_profile'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_view, name='logout_view'),
