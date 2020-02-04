a916b052b3b0f262ca33e77e5735cb0e08ac1589
Robin Muller <robin.muller.romutech@gmail.com>
2020-02-04 05:45:30 +0100

    feature: wip

--- social/models.py
+++ social/models.py
@@ -14,7 +14,7 @@ class Profile(models.Model):
         verbose_name="Profil"
 
     def __str__(self):
-        return "Profil de {}".format(self.user)
+        return "Profil de {}".format(User_Auth.objects.get(id=self.user))
 
 class Message(models.Model):
     content = models.CharField(max_length=100, verbose_name="")
@@ -39,3 +39,33 @@ class Comment(Message):
 
     def __str__(self):
         return self.content
+
+
+def save_status(request):
+    try:
+        if request.POST['type_form'] == 'profile_status':
+            status = ProfileStatus()
+            status.content = request.POST['content']
+            status.profile = Profile(id=request.POST['profile'])
+            status.author = request.user.id
+            status.save()
+            return True
+    except:
+        pass
+
+    return False
+
+def save_comment(request, form):
+    try:  
+        if request.POST['type_form'] == 'comment_status':
+            if form.is_valid():
+                comment = Comment()
+                comment.content = request.POST['content']
+                comment.status = ProfileStatus.objects.get(id=request.POST['status'])
+                comment.author = request.user.id
+                comment.save()
+                return True
+    except:
+        pass
+
+    return False
--- social/views.py
+++ social/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.http import Http404
-from .models import Profile, ProfileStatus, Comment
+from .models import Profile, ProfileStatus, save_status, save_comment
 from social.forms import CommentForm, ProfileForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User as User_Auth
@@ -15,33 +15,12 @@ def show_profile(request, id):
         profile = Profile.objects.get(id=id)
     except:
         raise Http404
-
-    try:    
-        if request.POST['type_form'] == 'profile_status':
-            status = ProfileStatus()
-            status.content = request.POST['content']
-            status.profile = Profile.objects.get(id=request.POST['profile'])
-            status.author = User_Auth.objects.get(id=request.user.id)
-            status.save()
-            return redirect(show_profile, id)
-    except:
-        pass
-
-    form = CommentForm(request.POST or None) 
-
-    try:  
-        if request.POST['type_form'] == 'comment_status':
-            if form.is_valid():
-                comment = Comment()
-                comment.content = request.POST['content']
-                comment.status = ProfileStatus.objects.get(id=request.POST['status'])
-                comment.author = User_Auth.objects.get(id=request.user.id)
-                comment.save()
-                return redirect(show_profile, id)
-    except:
-        pass
-
     statutes = ProfileStatus.objects.filter(profile=profile)
+    form = CommentForm(request.POST or None)
+    if save_status(request):
+        return redirect(show_profile, id)
+    elif save_comment(request, form):
+        return redirect(show_profile, id)
     return render(request, 'social/show_profile.html', locals())
 
 def edit_profile(request):
@@ -49,19 +28,13 @@ def edit_profile(request):
 
 def create_profile(request):
     form = ProfileForm(request.POST or None)
-
-
     user = User_Auth.objects.get(id=request.user.id)
-
     ref = User_Auth.objects.all()
-
     if form.is_valid():
         profile = Profile()
         profile.user = user
-      
         profile.save()
         return redirect(list_profiles)
-        
     return render(request, 'social/create_profile.html', locals())
 
 
@@ -73,6 +46,9 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
+            profile = Profile()
+            profile.user = user.id
+            profile.save()
             login(request, user)
             return redirect(list_profiles)
     else:
